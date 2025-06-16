38ede4ac06c2f363851df7b106b57ca8339ccc1b
VeeraBramham Penudota <141214605+veerupenudhota@users.noreply.github.com>
2025-06-16 14:44:42 +0530

    Update WeatherService.java

--- src/main/java/com/pvb/springboot/veeru/weather/service/WeatherService.java
+++ src/main/java/com/pvb/springboot/veeru/weather/service/WeatherService.java
@@ -6,7 +6,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
-    private final String API_KEY = "4285ab739d3f23a8c80e800a6086e46b"; // your real key
+    private final String API_KEY = "4285ab739d3f23a8c80e800a6086e46b";
 
     public String getWeatherByCity(String city) {
         String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +
